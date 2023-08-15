0d4efcbc6ddb1016f03996f853326044de5a82bd
RafaelAdrianMejia <tucsonrafa@outlook.com>
2023-08-15 15:47:12 -0700

    exercise completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject(); 
+        }
+
+        public static void FavSubject()
+        {
+            Console.Write("Enter your favorite subject: ");
+            string subject = Console.ReadLine();
+
+            switch(subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I hate math");
+                    break;
+                case "science":
+                    Console.WriteLine("I love science");
+                    break;
+                case "pe":
+                case "p.e.":
+                case "physical science":
+                case "phys ed":
+                    Console.WriteLine("P.E. is awesome");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite");
+                    break;
+                case "english":
+                    Console.WriteLine("I speak english");
+                    break;
+                default:
+                    Console.WriteLine("I forgot about that one");
+                    break;
+
+            }
         }
     }
 }
